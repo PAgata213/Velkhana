9268098db7da68fd55a1c56e18e6c28f61558d49
Patryk <ricochet852@gmail.com>
2023-03-02 18:49:57 +0100

    Initial commit

---
+++ Velkhana/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Velkhana.Server.Models;
+
+namespace Velkhana.Server.Data {
+  public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser> {
+    public ApplicationDbContext(
+        DbContextOptions options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions) {
+    }
+  }
+}
---
+++ Velkhana/Server/Models/ApplicationUser.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Velkhana.Server.Models {
+  public class ApplicationUser : IdentityUser {
+  }
+}
